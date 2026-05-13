883adbc99d1600ac09023e8ffcf4d0b7d0b40083
Nakshatra-thange <nakshthange@gmail.com>
2026-05-13 18:54:53 +0530

    >>

--- server/src/lib/prisma.js
+++ server/src/lib/prisma.js
@@ -1,7 +1,12 @@
-import pkg from "@prisma/client";
+import { PrismaClient } from "../../generated/prisma/client.ts";
+import { PrismaPg } from "@prisma/adapter-pg";
 
-const { PrismaClient } = pkg;
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  adapter,
+});
 
 export default prisma;
