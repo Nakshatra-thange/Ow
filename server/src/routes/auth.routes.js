67aa27eb5e2c462f87361bb4e2c6e96cc6ad8200
Nakshatra-thange <nakshthange@gmail.com>
2026-05-13 17:35:26 +0530

    >>

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -4,11 +4,17 @@ import {
     registerUser,
     loginUser,
   } from "../controllers/auth.controller.js";
+import authMiddleware from "../middleware/auth.middleware.js";
 
-  
 const router = express.Router();
 
 
 router.post('/register',registerUser);
 router.post('/login',loginUser);
+router.get("/me", authMiddleware, (req, res) => {
+    res.json({
+      message: "Protected route accessed",
+      user: req.user,
+    });
+  });
 export default router;
