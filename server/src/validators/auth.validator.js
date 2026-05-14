63f109a4e10e7f4dd2d353e09f9dcb60a54c0fe6
Nakshatra-thange <nakshthange@gmail.com>
2026-05-14 16:17:25 +0530

    ..

--- server/src/validators/auth.validator.js
+++ server/src/validators/auth.validator.js
@@ -1,11 +1,25 @@
 import { z } from "zod";
 
-export const createGroupSchema = z.object({
+export const registerSchema = z.object({
   name: z
     .string()
-    .min(2, "Group name too short"),
+    .min(2, "Name must be at least 2 characters"),
 
-  description: z
+  email: z
     .string()
-    .optional(),
+    .email("Invalid email"),
+
+  password: z
+    .string()
+    .min(6, "Password must be at least 6 characters"),
+});
+
+export const loginSchema = z.object({
+  email: z
+    .string()
+    .email("Invalid email"),
+
+  password: z
+    .string()
+    .min(6, "Password must be at least 6 characters"),
 });
